3a52cfc477195e141f98f35bf5c8f82245155292
Kirk Stennett <kirk@Kirks-MacBook-Air.local>
2016-10-24 13:24:57 -0700

    modify to allow any json requests

--- app/controllers/longboards_controller.rb
+++ app/controllers/longboards_controller.rb
@@ -1,4 +1,5 @@
 class LongboardsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   before_action :set_longboard, only: [:show, :edit, :update, :destroy]
 
   # GET /longboards
